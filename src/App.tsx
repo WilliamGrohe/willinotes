fbefe55f4093af4e6d713dd7cf6849279a48e13b
WilliamGrohe <william.grohe@gmail.com>
2024-02-09 03:30:53 -0300

    Refatorar a exibição das notas controlando atraves do state

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
+import { useState } from "react";
 import logo from "./assets/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
+interface Note {
+  id: string
+  date: Date
+  content: string
+}
+
 export function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  function onNoteCreated(content: string){
+    const newNote = {
+      id: crypto.randomUUID(), 
+      date: new Date(), 
+      content
+    }
+
+    setNotes([newNote, ...notes]);
+  }
+
   return (
     <>
       <div className="mx-auto max-w-6xl my-12 space-y-6">
         <img src={logo} alt="logo nlw expert" />
 
         <form className="w-full">
-          <input 
-            type="text" 
+          <input
+            type="text"
             placeholder="Busque em suas notas..."
             className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
           />
@@ -19,14 +38,11 @@ export function App() {
         <div className="h-px bg-slate-700" />
 
         <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-          <NewNoteCard />
+          <NewNoteCard onNoteCreated={onNoteCreated} />
 
-          <NoteCard note={
-            {date:new Date(), content:"Hello World"}
-          }/>
-
-          
-
+          {notes.map((note) => {
+            return <NoteCard key={note.id} note={note} />
+          })}
         </div>
       </div>
     </>
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,7 +1,7 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
-import { X } from "lucide-react"
+import { X } from "lucide-react";
 
 interface NoteCardProps {
   note: {
@@ -15,7 +15,10 @@ export function NoteCard(props: NoteCardProps) {
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md text-left flex flex-col bg-slate-800 gap-3 p-5 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
         <span className="text-sm font-medium text-slate-300">
-          {props.note.date.toISOString()}
+          {formatDistanceToNow(props.note.date, {
+            locale: ptBR,
+            addSuffix: true,
+          })}
         </span>
         <p className="text-sm leading-6 text-slate-400">{props.note.content}</p>
 
@@ -27,7 +30,7 @@ export function NoteCard(props: NoteCardProps) {
         <Dialog.Overlay className="inset-0 fixed bg-black/60" />
         <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
           <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
-            <X className="size-5"/>
+            <X className="size-5" />
           </Dialog.Close>
 
           <div className="flex flex-1 flex-col gap-3 p-5">
@@ -46,7 +49,11 @@ export function NoteCard(props: NoteCardProps) {
             type="button"
             className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
           >
-            Deseja <span className="text-red-400 group-hover:underline">apagar esta nota</span>?
+            Deseja{" "}
+            <span className="text-red-400 group-hover:underline">
+              apagar esta nota
+            </span>
+            ?
           </button>
         </Dialog.Content>
       </Dialog.Portal>
