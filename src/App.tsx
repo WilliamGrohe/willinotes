458ba85000ae087475ad09d0a9a128c1762f6177
WilliamGrohe <william.grohe@gmail.com>
2024-02-09 03:45:40 -0300

    Salvar as notas em localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,15 @@ interface Note {
 }
 
 export function App() {
-  const [notes, setNotes] = useState<Note[]>([]);
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem('notes')
+
+    if (notesOnStorage) {
+      return JSON.parse(notesOnStorage)
+    }
+
+    return []
+  });
 
   function onNoteCreated(content: string){
     const newNote = {
@@ -18,8 +26,12 @@ export function App() {
       date: new Date(), 
       content
     }
+    
+    const notesArray = [newNote, ...notes]
+    
+    setNotes(notesArray)
 
-    setNotes([newNote, ...notes]);
+    localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
   return (
