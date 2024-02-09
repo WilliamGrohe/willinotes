f7eb2678374fab46f47a12b4a5caae5c810bcb69
WilliamGrohe <william.grohe@gmail.com>
2024-02-09 03:57:42 -0300

    Adicionar função ao campo de busca e retornar filtros

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,73 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import logo from "./assets/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
 interface Note {
-  id: string
-  date: Date
-  content: string
+  id: string;
+  date: Date;
+  content: string;
 }
 
 export function App() {
+  const [search, setSearch] = useState("");
   const [notes, setNotes] = useState<Note[]>(() => {
-    const notesOnStorage = localStorage.getItem('notes')
+    const notesOnStorage = localStorage.getItem("notes");
 
     if (notesOnStorage) {
-      return JSON.parse(notesOnStorage)
+      return JSON.parse(notesOnStorage);
     }
 
-    return []
+    return [];
   });
 
-  function onNoteCreated(content: string){
+  function onNoteCreated(content: string) {
     const newNote = {
-      id: crypto.randomUUID(), 
-      date: new Date(), 
-      content
-    }
-    
-    const notesArray = [newNote, ...notes]
-    
-    setNotes(notesArray)
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    };
 
-    localStorage.setItem('notes', JSON.stringify(notesArray))
+    const notesArray = [newNote, ...notes];
+
+    setNotes(notesArray);
+
+    localStorage.setItem("notes", JSON.stringify(notesArray));
   }
 
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value;
+
+    setSearch(query);
+  }
+
+  const filteredNotes =
+    search !== ""
+      ? notes.filter((note) => note.content.includes(search))
+      : notes;
+
   return (
-    <>
-      <div className="mx-auto max-w-6xl my-12 space-y-6">
-        <img src={logo} alt="logo nlw expert" />
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="logo nlw expert" />
 
-        <form className="w-full">
-          <input
-            type="text"
-            placeholder="Busque em suas notas..."
-            className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
-          />
-        </form>
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+          onChange={handleSearch}
+        />
+      </form>
 
-        <div className="h-px bg-slate-700" />
+      <div className="h-px bg-slate-700" />
 
-        <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-          <NewNoteCard onNoteCreated={onNoteCreated} />
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard onNoteCreated={onNoteCreated} />
 
-          {notes.map((note) => {
-            return <NoteCard key={note.id} note={note} />
-          })}
-        </div>
+        {filteredNotes.map((note) => {
+          return <NoteCard key={note.id} note={note} />;
+        })}
       </div>
-    </>
+    </div>
   );
 }
