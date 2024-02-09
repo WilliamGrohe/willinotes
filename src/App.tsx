68f86d5dcdbfe034592e2f513e6ca802883609f4
WilliamGrohe <william.grohe@gmail.com>
2024-02-09 04:34:35 -0300

    Alterar logo e favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, useState } from "react";
-import logo from "./assets/logo-nlw-expert.svg";
+import logo from "./assets/willinotes-logo.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
@@ -48,7 +48,7 @@ export function App() {
 
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
-      <img src={logo} alt="logo nlw expert" />
+      <img src={logo} alt="logo nlw expert" sizes="950px"/>
 
       <form className="w-full">
         <input
